d76293a08a6cc5238e2bca0a918f9d3fd7339f5d
hem1587 <hemantchaudhari1587@gmail.com>
2024-05-13 15:03:07 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gray-100 p-8">
+    <h1 className="text-2xl font-bold text-red-800">Hello Tailwind!</h1>
+    <p className="text-gray-600 mt-2">Tailwind CSS is awesome!</p>
+  </div>
+  );
+}
+
+export default App;
